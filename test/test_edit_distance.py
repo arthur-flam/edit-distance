c43544f9789c935076266f7031354068e8479dcd
Arthur Flam <arthur.flam@gmail.com>
2016-01-06 15:20:02 +0200

    added tests for empty/equal strings

--- test/test_edit_distance.py
+++ test/test_edit_distance.py
@@ -2,6 +2,10 @@ import unittest
 from edit_distance import edit_distance
 
 class TestEditDistance(unittest.TestCase):
+    def test_equal(self):
+        "handles equal strings"
+        self.assertEqual(edit_distance("azerty", "azerty"), 0)
+
     def test_delete(self):
         "able to find a simple delete"
         self.assertEqual(edit_distance("aaab", "aaa"), 1)
@@ -13,3 +17,9 @@ class TestEditDistance(unittest.TestCase):
     def test_change(self):
         "able to find a simple change"
         self.assertEqual(edit_distance("hat", "cat"), 1)
+
+    def test_empty(self):
+        "handles empty strings"
+        self.assertEqual(edit_distance("", "a"), 1)
+        self.assertEqual(edit_distance("a", ""), 1)
+        self.assertEqual(edit_distance("", ""), 0)
